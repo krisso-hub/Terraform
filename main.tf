45d00acc2214a55cfdc5b1d6f5ee914e5d8e7ebb
krisso-hub <krissogwa@mail.com>
2021-11-17 22:49:20 -0500

    ec2 added

--- main.tf
+++ main.tf
@@ -3,6 +3,23 @@ provider "aws" {
     region  = "us-east-1"
 }
 
-resources aws_instance ec2{
-    
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
